e4ca1e222ae60cdead610920cbf7b59b19e0235b
Jeffrey Aven (DPC) <jeffrey.aven@dpc.vic.gov.au>
2021-12-06 08:39:44 +1100

    updated config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,20 +1,25 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
+const TwitterSvg =
+  '<svg style="fill: #1DA1F2; vertical-align: middle;" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path></svg>';
+
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'StackQL',
+  tagline: 'Provision and Query Cloud and SaaS Resources using SQL',
+  url: 'https://stackql.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'stackql', 
+  projectName: 'stackql.io', 
+
+  plugins: [],
 
   presets: [
     [
@@ -23,17 +28,27 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          path: 'docs',
+		  sidebarCollapsible: true, 
+          editUrl: 'https://github.com/stackql/stackql.io/edit/main/',
         },
         blog: {
+          path: 'blog',
+          blogTitle: 'StackQL Blog',
+          blogDescription: 'Cloud operations, security and automation using SQL',
+          postsPerPage: 5,
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
+          feedOptions: {
+            type: 'all',
+            title: 'StackQL Blog Feed',
+            description: 'Cloud operations, security and automation using SQL',
+            copyright: `Copyright © ${new Date().getFullYear()} StackQL Studios`,
+          },
           showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/css/global.css'),
         },
       }),
     ],
@@ -42,77 +57,160 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+    announcementBar: {
+      id: 'support_us',
+      content:
+        `⭐️ <b>If you like StackQL, give it a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/facebook/docusaurus">GitHub</a> and follow us on <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/docusaurus" >Twitter</a></b> ${TwitterSvg}`,
+      backgroundColor: '#A9BCD0',
+      textColor: '#1A4E82',
+      isCloseable: true,
+    },    
+    //smartlook: {
+    //  projectKey: '2c78d968d0285f6d1d8f41008505e629a0a830dc',
+    //},    
+    //gtag: {
+    //  trackingID: 'G-W3ERTBY7LQ',
+    //  anonymizeIP: false,
+    //}, 
+    //algolia: {
+    //  apiKey: 'e656729c343b12a0507b97b9aacf78bb',
+    //  indexName: 'stackql',
+      // Optional: see doc section below
+      // contextualSearch: false,
+      // Optional: see doc section below
+      // appId: 'YOUR_APP_ID',
+      // Optional: Algolia search parameters
+    //  searchParameters: {},
+      //... other Algolia params
+    //},    
+    // sidebarCollapsible: true, 
+    hideableSidebar: true,  
+    navbar: {
+      logo: {
+        alt: 'StackQL',
+        href: '/',
+        //target: '_self',
+        src: 'img/logo-original.svg',
+        srcDark: 'img/logo-white.svg',
+      },
+      items: [
+        {
+          href: 'https://stackql.io/features',
+          label: 'Features',
+          position: 'left',
+          target: '_self',
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+        {
+          href: 'https://stackql.io/contact-us',
+          label: 'Contact us',
+          position: 'left',
+          target: '_self',
+        },
+        {
+          href: 'https://stackql.io/downloads',
+          label: 'Downloads',
+          position: 'left',
+          target: '_self',
+        },        
+        {
+          to: 'docs',
+          activeBaseRegex: '^((?!/blog).)*$',
+          label: 'Documentation',
+          position: 'left',
+        },
+        {
+          to: 'blog',
+          label: 'Blog',
+          activeBasePath: 'blog',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/stackql',
+          position: 'right',
+          className: 'header-github-link',
+          'aria-label': 'GitHub repository',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      logo: {
+        alt: 'StackQL',
+        href: 'https://stackql.io/',
+        src: 'img/logo-original.svg',
+        srcDark: 'img/logo-white.svg',
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+      links: [
+        {
+          title: 'Pages',
+          items: [
+            {
+              label: 'Features',
+              href: 'https://stackql.io/features/',
+              target: '_self',
+            },
+            {
+              label: 'Downloads',
+              href: 'https://stackql.io/downloads/',
+              target: '_self',
+            },
+            {
+              label: 'Contact us',
+              href: 'https://stackql.io/contact-us/',
+              target: '_self',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Github',
+              href: 'https://github.com/stackql',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/stackql',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/stackql',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Documentation',
+              to: '/',
+            },
+            {
+              label: 'Blogs',
+              to: 'blog',
+            },
+            {
+              label: 'Tutorials',
+              to: '/',
+            },
+            {
+              label: 'Videos',
+              href: 'https://youtube.com',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `<strong>© ${new Date().getFullYear()} StackQL Studios</strong>`,
+    },
+    colorMode: {
+      // using user system preferences, instead of the hardcoded defaultMode
+      respectPrefersColorScheme: true,
+    },		
+	}),
 };
 
 module.exports = config;
