3306fbc1b0c5bf68064371e789ce90ec4a9cb73d
Jeffrey Aven (DPC) <jeffrey.aven@dpc.vic.gov.au>
2022-01-07 11:59:01 +1100

    updated animated term styling

--- src/components/AnimatedTerm/AnimatedTerm.js
+++ src/components/AnimatedTerm/AnimatedTerm.js
@@ -5,19 +5,23 @@ import Code from '../Code';
 const AnimatedTerm = props => {
     const { data } = props;
     return (
-        <div className={[styles.marvelDevice, styles.macbook].join(" ")}>
-            <div className={styles.notch}>
-                <div className={styles.camera}></div>
-                <div className={styles.speaker}></div>
+        <section className={[styles.macSection,].join(" ")}>
+            <div className={[styles.macDiv,].join(" ")}>
+                <div className={[styles.marvelDevice, styles.macbook,].join(" ")}>
+                    <div className={styles.notch}>
+                        <div className={styles.camera}></div>
+                        <div className={styles.speaker}></div>
+                    </div>
+                    <div className={styles.topBar}></div>
+                    <div className={styles.sleep}></div>
+                    <div className={styles.bottomBar}></div>
+                    <div className={styles.volume}></div>
+                    <div className={styles.screen}>
+                        <Code data={data} />
+                    </div>
+                </div>
             </div>
-            <div className={styles.topBar}></div>
-            <div className={styles.sleep}></div>
-            <div className={styles.bottomBar}></div>
-            <div className={styles.volume}></div>
-            <div className={styles.screen}>
-                <Code data={data} />
-            </div>
-        </div>
+        </section>
     );
 };
     
