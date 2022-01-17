08d8038cf6a91ebc42b1b2ac59305dfcaed102f5
Jeffrey Aven (DPC) <jeffrey.aven@dpc.vic.gov.au>
2022-01-17 14:06:55 +1100

    updates

--- src/components/HubspotContactForm/HubspotContactForm.js
+++ src/components/HubspotContactForm/HubspotContactForm.js
@@ -1,31 +1,20 @@
-import React, {useEffect} from "react";
+import React from 'react';
 
 const HubspotContactForm = props => {
     const { region, portalId, formId } = props;
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src='https://js.hsforms.net/forms/shell.js';
-        document.body.appendChild(script);
-
-        script.addEventListener('load', () => {
-            // @TS-ignore
-            if (window.hbspt) {
-                // @TS-ignore
-                window.hbspt.forms.create({
-                    region: {region},
-                    portalId: {portalId},
-                    formId: {formId},
-                    target: '#hubspotForm'
-                })
-            }
-        });
-    }, []);
-
+    const formCode = `
+    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+    <script>
+      hbspt.forms.create({
+        region: "${region}",
+        portalId: "${portalId}",
+        formId: "${formId}"
+    });
+    </script>
+    `;
     return (
-        <div>
-            <div id="hubspotForm"></div>
-        </div>
-    );
+        <div id="hubspotForm" dangerouslySetInnerHTML={{__html: formCode}} />
+  );
 };
 
 export default HubspotContactForm;
