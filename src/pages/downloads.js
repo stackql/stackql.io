09e1315468184fd8ac74de1db105cbb870ddcedd
Jeffrey Aven (DPC) <jeffrey.aven@dpc.vic.gov.au>
2022-01-18 14:34:05 +1100

    downloads page formatting

--- src/pages/downloads.js
+++ src/pages/downloads.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import {
   SectionHeader,
-  DownloadLink,
   DocumentationLink,
   DownloadCard,
 } from '../components';
-import Link from '@docusaurus/Link';
+import { useMediaQuery } from 'react-responsive'
 import MediaQuery from 'react-responsive'
+
+import buttonStyles from '../components/Hero/hero.module.css';
 
 import { downloadsPageData } from '../data/downloads';
 const windows = downloadsPageData.downloadCards.windows;
@@ -17,7 +17,19 @@ const macos = downloadsPageData.downloadCards.macos;
 const linux = downloadsPageData.downloadCards.linux;
 
 export default function Downloads() {
-  const {siteConfig} = useDocusaurusContext();
+
+  const isNormalLayout = useMediaQuery({
+    query: '(min-width: 997px)'
+  });
+
+  const isMobile = useMediaQuery({
+    query: '(max-width: 480px)'
+  });
+
+  const isTablet = useMediaQuery({
+    query: '(min-width: 481px) and (max-width: 996px)'
+  });
+
   return (
    <Layout
    title={downloadsPageData.title}
@@ -31,28 +43,49 @@ export default function Downloads() {
           subtitle={downloadsPageData.header.subtitle}
           align="center"
           ctaGroup = {[
-          ]}
+            <div className={clsx(buttonStyles.buttons)}>
+            <DocumentationLink />
+            </div>
+        ]}
         />
       </header>
-     
       <main>
-        <div className={clsx('padding-bottom--xl', 'lgContainer', 'divHalfBackgroundBottom')}>
+        <div className={clsx('padding-bottom--xl', 'lgContainer', isNormalLayout ? 'divHalfBackgroundBottom' : '')}>
           <div className={clsx('row')}>
-            <div className={clsx('container')}>
+            <div className={clsx(isNormalLayout ? 'container' : isTablet ? 'tabletContainer' : 'mobileContainer')}>
+            {/* desktop/large tablet view */}         
+            <MediaQuery minWidth={997}>
               <div className={clsx('row')}>
               {[windows, macos, linux].map(card => (
-                <div className={clsx('col', 'col--4', 'margin-bottom--md')}>
-                <DownloadCard
-                    key={card.title}
-                    data={card}
-                    liftUp
-                  />
-                  </div>
+                <div className={clsx('col', 'col--4')}>
+                  <DownloadCard
+                      key={card.title}
+                      data={card}
+                      liftUp
+                    />
+                </div>
                 ))}
-              </div>          
+              </div> 
+            </MediaQuery>
+
+          {/* mobile/small tablet view */}                
+            <MediaQuery maxWidth={996}>
+            {[windows, macos, linux].map(card => (
+              <div className={clsx('row')}>
+                  <div className={clsx('col', 'col--12', 'margin-bottom--lg')}>
+                    <DownloadCard
+                        key={card.title}
+                        data={card}
+                        liftUp
+                      />
+                  </div>
+              </div>
+              ))}
+            </MediaQuery>
+
             </div>
           </div>
-        </div>  
+        </div>
       </main>
     </Layout>
   );
