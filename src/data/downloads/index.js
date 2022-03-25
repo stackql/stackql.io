5f86c81310dc335f0e89928b53d55c4f10b4769f
jeffreyaven <javen@avensolutions.com>
2022-03-25 19:29:50 +1100

    fixed mobile rendering for downloads

--- src/data/downloads/index.js
+++ src/data/downloads/index.js
@@ -22,47 +22,50 @@ export const downloadsPageData = {
         subtitle: "A new approach to querying and provisioning cloud services.",
         label: "FAMILIAR, FUNCTIONAL, FLEXIBLE",
     },
-    downloadCards: {
-        windows: {
-            icon: 'fab fa-windows',
-            title: 'Microsoft Windows',
-            description: 'x86 and x64',
-            buttons: [
-              {
-                url:  'https://releases.stackql.io/stackql/latest/infraql_windows_amd64.msi',
-                text: 'Download MSI',
-                icon: 'fas fa-download',
-              },
-              {
-                url:  'https://releases.stackql.io/stackql/latest/infraql_windows_amd64.zip',
-                text: 'Download ZIP',
-                icon: 'fas fa-download',
-              },    
-            ],
+    downloadCards: [
+        {
+          platform: 'windows',
+          icon: 'fab fa-windows',
+          title: 'Microsoft Windows',
+          description: 'x86 and x64',
+          buttons: [
+            {
+              url:  'https://releases.stackql.io/stackql/latest/infraql_windows_amd64.msi',
+              text: 'Download MSI',
+              icon: 'fas fa-download',
+            },
+            {
+              url:  'https://releases.stackql.io/stackql/latest/infraql_windows_amd64.zip',
+              text: 'Download ZIP',
+              icon: 'fas fa-download',
+            },    
+          ],
         },
-        macos: {
-            icon: 'fab fa-apple',
-            title: 'MacOS',
-            description: '64-bit AMD and ARM',
-            buttons: [
-              {
-                url:  'https://releases.stackql.io/stackql/latest/stackql_darwin_multiarch.pkg',
-                text: 'Download PKG',
-                icon: 'fas fa-download',
-              },
-            ],
+        {
+          platform: 'macos',  
+          icon: 'fab fa-apple',
+          title: 'MacOS',
+          description: '64-bit AMD and ARM',
+          buttons: [
+            {
+              url:  'https://releases.stackql.io/stackql/latest/stackql_darwin_multiarch.pkg',
+              text: 'Download PKG',
+              icon: 'fas fa-download',
+            },
+          ],
         },
-        linux: {
-            icon: 'fab fa-linux',
-            title: 'Linux',
-            description: 'x86 and x64',
-            buttons: [
-              {
-                url:  'https://releases.stackql.io/stackql/latest/stackql_linux_amd64.zip',
-                text: 'Download ZIP',
-                icon: 'fas fa-download',
-              },
-            ]
+        {
+          platform: 'linux',  
+          icon: 'fab fa-linux',
+          title: 'Linux',
+          description: 'x86 and x64',
+          buttons: [
+            {
+              url:  'https://releases.stackql.io/stackql/latest/stackql_linux_amd64.zip',
+              text: 'Download ZIP',
+              icon: 'fas fa-download',
+            },
+          ]
         }
-    }
+      ]
 };
