1e5851c02eb33d3e520b61023e2d7a5411e37e82
Jeffrey Aven <javen@stackql.io>
2025-09-18 19:37:29 +1000

    moved k8s and homebrew to microsites

--- src/pages/providers/k8s.js
+++ src/pages/providers/k8s.js
@@ -4,7 +4,7 @@ import Head from '@docusaurus/Head';
 export default function Registry() {
   return (
     <Head>
-    <meta http-equiv="refresh" content="0;URL='https://k8s.stackql.io/providers/k8s/'" />
+    <meta http-equiv="refresh" content="0;URL='https://k8s-provider.stackql.io/'" />
     </Head>
   );
 };
